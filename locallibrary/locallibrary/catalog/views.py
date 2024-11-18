b6d7a83b6f3d6372f5e3405165e38aa4ece54630
Andrew Star <starovoytov.andryusha00@mail.ru>
2024-11-18 19:26:11 +0700

    Список_книг

--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book, Author, BookInstance, Genre
+from .models import Book, Author, BookInstance
+from django.views import generic
 
 def index(request):
     """
@@ -16,6 +17,20 @@ def index(request):
     # переменной контекста context
     return render(
         request,
-        'index.html',
+        'catalog/index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'catalog/book_list.html'  # Убедитесь, что шаблон существует
+    context_object_name = 'book_list'  # Используемое имя для списка книг в шаблоне
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'catalog/book_detail.html'  # Убедитесь, что шаблон существует
+    context_object_name = 'book'  # Имя переменной, которая будет передана в шаблон
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
